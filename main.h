7775a640cd72c20bc0522cfd1eb93e1457d1d445
Gemestia <143001422+Ngoctay12@users.noreply.github.com>
2024-03-13 19:15:06 +0700

    Add files via upload

---
+++ main.h
@@ -0,0 +1,25 @@
+
+#ifndef _MAIN_1_H_
+#define _MAIN_1_H_
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+//The library here is concretely set, students are not allowed to include any other libraries.
+
+
+#define folder_input  "TestCase/input/input"
+#define folder_output "TestCase/output/output"
+#define folder_expect "TestCase/expect/expect"
+
+
+
+#endif //_MAIN_1_H_
